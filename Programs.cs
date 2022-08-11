8ec5c8ae8b083e266cdfcf27a2855743c85c30fe
Mikhail <mikhailpanov1995@example.com>
2022-08-11 12:38:31 +0700

    Первая задача

---
+++ Programs.cs
@@ -0,0 +1,15 @@
+﻿{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int max = 0;
+    if (a > b)
+    {    
+        max = a;
+    }
+    else 
+    {
+        max = b;
+    }
+
+    Console.WriteLine(max);
+}
